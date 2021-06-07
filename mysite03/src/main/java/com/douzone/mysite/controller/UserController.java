35b02b3b7f23030f186b31286a00aa669ce211dc
BIT <mani703@naver.com>
2021-06-07 10:59:47 +0900

    Annotation

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.douzone.mysite.security.Auth;
 import com.douzone.mysite.service.UserService;
 import com.douzone.mysite.vo.UserVo;
 
@@ -75,6 +76,7 @@ public class UserController {
 		return "redirect:/";
 	}
 	
+	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.GET)
 	public String update(HttpSession session, Model model) {
 		//접근제어
@@ -90,6 +92,7 @@ public class UserController {
 		return "user/update";
 	}
 	
+	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public String update(HttpSession session, UserVo userVo) {
 		//접근제어
