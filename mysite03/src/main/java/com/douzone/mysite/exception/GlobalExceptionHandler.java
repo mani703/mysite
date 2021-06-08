a2007b23d66fcc3eb7197b26cc87dee2ab2fedce
BIT <mani703@naver.com>
2021-06-08 10:45:59 +0900

    logging #

--- mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/douzone/mysite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.douzone.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,12 +12,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
+	private static final Log LOGGER = LogFactory.getLog(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception e) {
-		// 1. 로깅(loggin)
+		// 1. 로깅(logging)
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors);
+//		System.out.println(errors);
+		/**
+		 * 1. appenger
+		 *    - file apeender /log-mysite/exception.?.log.zip
+		 *      10M (Archiving 정책)
+		 *      1-10(rolling)
+		 *    - console appender
+		 *    
+		 * 2. logger - com.douzone.mysite.exception, level(error), (console+file)
+		 *    logger - Root, level(debug), console appender
+		 * 
+		 */
+		LOGGER.error(errors.toString());
+		
 		// 2. 사과 페이지
 		// 3. 정상 종료
 		
